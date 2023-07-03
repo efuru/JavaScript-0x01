fa577fe64241fbf59d89be261c0b7eed1907d57a
efuruanthonytech@yahoo.com <117752260+efuru@users.noreply.github.com>
2023-07-03 16:20:56 -0700

    hello, World! in JS

---
+++ part1-js/hello.js
@@ -0,0 +1,9 @@
+console.log("Efuru Anthony");
+console.log("1992");
+console.log(1992);
+console.log("Eruru Anthony (1992)");
+console.log("Efuru, Anthony, (1992)");
+console.log("Efuru");
+console.log("Anthony");
+console.log("1992");
+console.log("Efuru " + " Anthony " + " (1992)");
